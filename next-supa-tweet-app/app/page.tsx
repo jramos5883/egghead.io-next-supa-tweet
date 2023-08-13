a87104e2fb90afbb8a12fc6c559f0172fc88cd31
jramos5883 <jramos5883@gmail.com>
2023-08-13 12:55:10 -0700

    Finished building sign in and sign out function

--- next-supa-tweet-app/app/page.tsx
+++ next-supa-tweet-app/app/page.tsx
@@ -1,14 +1,15 @@
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
-import { cookies } from 'next/headers';
-import Image from 'next/image';
-
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import AuthButton from "./auth-button";
 
 export default async function Home() {
-
-const supabase = createServerComponentClient({ cookies });
-const { data: tweets } = await supabase.from('tweets').select();
+  const supabase = createServerComponentClient({ cookies });
+  const { data: tweets } = await supabase.from("tweets").select();
 
   return (
-    <pre>{JSON.stringify(tweets, null, 2)}</pre>
-  )
+    <>
+      <AuthButton />
+      <pre>{JSON.stringify(tweets, null, 2)}</pre>
+    </>
+  );
 }
