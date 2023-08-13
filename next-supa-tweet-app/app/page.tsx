aff93e7ba7b77a150ceb8823ec6a41dee164657d
jramos5883 <jramos5883@gmail.com>
2023-08-13 11:47:20 -0700

    Working on supabase twitter project

---
+++ next-supa-tweet-app/app/page.tsx
@@ -0,0 +1,14 @@
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
+import Image from 'next/image';
+
+
+export default async function Home() {
+
+const supabase = createServerComponentClient({ cookies });
+const { data: tweets } = await supabase.from('tweets').select();
+
+  return (
+    <pre>{JSON.stringify(tweets, null, 2)}</pre>
+  )
+}
